5dac5e170c8c0f3e3bba5eb1a7a93554de16bed2
unknown <MADDENJS@MVHS-CS15-14.dcsdk12.org>
2015-09-28 10:21:31 -0600

    Summarize complete, sigma started

--- number_summary.rb
+++ number_summary.rb
@@ -9,9 +9,9 @@ class NumberSummary
 			array = array.split ","
 			# arra = array.to_i
 			(0...array.length).each do |n|
-				array[n] = array[n].to_i
+				array[n] = array[n].to_f
 			end
-			array
+			array.sort
 		end
 
 		def max(array)
@@ -28,23 +28,41 @@ class NumberSummary
 			(0...array.length).each do |n|
 				sum += array[n]
 			end
-			(sum / array.length)
+			(sum / array.length).round(1)
 		end
 
 		def median(array)
-			array.sort
 			average = 0
 			if array.length % 2 == 0
 				average = (array[(array.length / 2 - 1)] + array[array.length / 2]) / 2
 			else 
 				return array[array.length / 2]
 			end
-			average
+			average.round(1)
 		end
 
 		def mode(array)
-			most = array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
-			array.max_by { |v| most[v] }
+			max = 0
+			array_uni = array.uniq
+			array_count = []
+			array_final = []
+			(0...array_uni.length).each do |n|
+				array_count.push(0)
+			end
+			(0...array.length).each do |n|
+				(0...array_uni.length).each do |x|
+					if array_uni[x] == array[n]
+						array_count[x] += 1
+					end
+				end
+			end
+			max = array_count.max
+			(0...array_count.length).each do |n|
+				if array_count[n] == max
+					array_final.push(array_uni[n])
+				end
+			end
+			array_final
 		end
 
 		def q1(array)
@@ -54,7 +72,7 @@ class NumberSummary
 				array_final = array[0..(array.length/2)]
 			end	
 			
-			median(array_final)
+			median(array_final).round(1)
 		end
 
 		def q3(array)
@@ -64,21 +82,35 @@ class NumberSummary
 				array_final = array[(array.length/2 + 1)...array.length]
 			end
 			
-			median(array_final)
+			median(array_final).round(1)
 		end
 
 		def sigma(array)
-			variation = mean(array)
-			puts variation
+			variation = mean(array).round(1)
 			array_dif = []
 			(0...array.length).each do |n|
-				array_dif.push((array[n] - variation)**2)
+				array_dif.push(((array[n] - variation)**2).round(1))
 			end	
-			mean(array_dif)
+			Math.sqrt(mean(array_dif))
+		end
+
+		def summarize(filename)
+			array = return_array(filename)
+			puts "Number Summary: "
+			puts "min: #{NumberSummary.min(NumberSummary.return_array("data.csv"))}"
+			puts "max: #{NumberSummary.max(NumberSummary.return_array("data.csv"))}"
+			puts "mean: #{NumberSummary.mean(NumberSummary.return_array("data.csv"))}"
+			puts "median: #{NumberSummary.median(NumberSummary.return_array("data.csv"))}"
+			puts "q1: #{NumberSummary.q1(NumberSummary.return_array("data.csv"))}"
+			puts "q3: #{NumberSummary.q3(NumberSummary.return_array("data.csv"))}"
+			puts "mode: #{NumberSummary.mode(NumberSummary.return_array("data.csv"))}"
+			puts "sigma: #{NumberSummary.sigma(NumberSummary.return_array("data.csv"))}"
 		end
 
 	end
 
 end
 
-puts NumberSummary.max(NumberSummary.return_array("data.csv"))
+NumberSummary.summarize("data.csv")
+
+
